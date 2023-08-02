1a3d75e55cc274c7273e29f3d2977e12e9283390
Christina Wu <wu_christina@outlook.com>
2023-08-02 18:39:27 +0000

    Internationalize the messages in the calculator

--- js-101/lesson-2/calculator-bonus-features.js
+++ js-101/lesson-2/calculator-bonus-features.js
@@ -5,42 +5,69 @@ function prompt(message) {
   console.log(`=> ${message}`);
 }
 
+function invalidLanguage(language) {
+  return language !== 'en' && language !== 'fr' && language !== 'es';
+}
+
 function invalidNumber(number) {
   return number.trimStart() === '' || Number.isNaN(Number(number));
 }
 
 function invalidAnswer(answer) {
-  return (answer !== 'y') && (answer !== 'n');
+  return (answer !== 'y') && (answer !== 'n') && (answer !== 'o') && (answer !== 's');
 }
 
-prompt(MESSAGES.welcome);
+function invalidDivision(number, operation) {
+  return number === '0' && operation === '4';
+}
 
-while (true) {
+function askLanguage() {
+  prompt(MESSAGES.languagePrompt);
+  let language = readline.question();
 
-  prompt(MESSAGES.firstNumber);
-  let number1 = readline.question();
-
-  while (invalidNumber(number1)) {
-    prompt(MESSAGES.validNumber);
-    number1 = readline.question();
+  while (invalidLanguage(language)) {
+    prompt(MESSAGES.validLanguage);
+    language = readline.question();
   }
+  return language;
+}
 
-  prompt(MESSAGES.secondNumber);
-  let number2 = readline.question();
+function askNumber(language, numberKey) {
+  prompt(MESSAGES[language][numberKey]);
+  let number = readline.question();
 
-  while (invalidNumber(number2)) {
-    prompt(MESSAGES.validNumber);
-    number2 = readline.question();
+  while (invalidNumber(number)) {
+    prompt(MESSAGES[language].validNumber);
+    number = readline.question();
   }
+  return number;
+}
 
-  prompt(MESSAGES.operations);
+function askOperation(language) {
+  prompt(MESSAGES[language].operations);
   let operation = readline.question();
 
   while (!['1', '2', '3', '4'].includes(operation)) {
-    prompt(MESSAGES.validOperation);
+    prompt(MESSAGES[language].validOperation);
     operation = readline.question();
   }
+  return operation;
+}
 
+function checkDivisionByZero(language, number2, operation) {
+  while (invalidDivision(number2, operation)) {
+    prompt(MESSAGES[language].invalidDivision);
+    number2 = readline.question();
+  }
+
+  while (invalidNumber(number2)) {
+    prompt(MESSAGES[language].validNumber);
+    number2 = readline.question();
+  }
+  return number2;
+}
+
+function calculatedValue(operation, number1, number2) {
   let output;
   switch (operation) {
     case '1':
@@ -56,17 +83,41 @@ while (true) {
       output = Number(number1) / Number(number2);
       break;
   }
+  return output;
+}
 
-  prompt(MESSAGES.result.concat(output));
-
-  prompt(MESSAGES.restart);
+function askToRestart(language) {
+  prompt(MESSAGES[language].restart);
   let restartAnswer = readline.question();
 
   while (invalidAnswer(restartAnswer)) {
-    prompt(MESSAGES.validAnswer);
+    prompt(MESSAGES[language].validAnswer);
     restartAnswer = readline.question();
   }
+  return restartAnswer;
+}
+
+
+// program begins
+
+let chosenLanguage = askLanguage();
+prompt(MESSAGES[chosenLanguage].welcome);
+
+while (true) {
+
+  let number1 = askNumber(chosenLanguage, "firstNumber");
+  let number2 = askNumber(chosenLanguage, "secondNumber");
+  let operation = askOperation(chosenLanguage);
+  number2 = checkDivisionByZero(chosenLanguage, number2, operation);
+
+  let finalValue = calculatedValue(operation, number1, number2);
+
+  prompt(MESSAGES[chosenLanguage].result.concat(finalValue));
+
+  let restartAnswer = askToRestart(chosenLanguage);
 
   if (restartAnswer === 'n') break;
-
+  console.clear();
 }
+
+console.clear();
